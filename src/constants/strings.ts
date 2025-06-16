a8b389a660ac961df9ed74b43f2dfc4e14a6d084
Tharun-999 <tharunnani617@gmail.com>
2025-06-16 12:38:55 -0500

    Updated portfolio site

---
+++ src/constants/strings.ts
@@ -0,0 +1,38 @@
+class Strings {
+  static telegram = "Telegram";
+  static telegramLink = "https://telegram.me/tharunkumarpanga";
+  static telegramUsername = "tharunkumarpanga";
+
+  static github = "GitHub";
+  static githubLink = "https://github.com/Tharun-999";
+  static githubUsername = "Tharun-999";
+
+  static linkedIn = "LinkedIn";
+  static linkedInLink = "https://www.linkedin.com/in/tharunkumarpanga/";
+  static linkedInUsername = "tharunkumar";
+
+  static instagram = "Instagram";
+  static instagramLink = "https://www.instagram.com/__tharun_nani";
+  static instagramUsername = "__tharun_nani";
+
+  static twitter = "Twitter";
+  static twitterLink = "https://x.com/tharun_panga";
+  static twitterUsername = "tharun_panga";
+
+  static email = "Email";
+  static primaryEmailLink = "mailto:tharunkumarpanga@gmail.com";
+  static primaryEmail = "tharunkumarpanga@gmail.com";
+
+  static buyMeACoffee = "BuyMeACoffee";
+  static buyMeACoffeeLink = "https://www.buymeacoffee.com/tharunpanga";
+  static buyMeACoffeeUsername = "tharunpanga";
+
+  static koFi = "Ko-Fi";
+  static koFiLink = "https://ko-fi.com/tharunpanga";
+  static koFiUsername = "tharunpanga";
+
+  static fullName = "Tharun Panga";
+  static shortName = "Tharun";
+}
+
+export default Strings;
---
+++ src/data/experiences.ts
@@ -0,0 +1,62 @@
+import { IExperienceItem } from "@/types";
+
+const experiences: IExperienceItem[] = [
+  {
+    designation: "Software Development Engineer",
+    company: "Tulboxx.ai",
+    startDate: "Apr 2025",
+    isCurrentJob: true,
+    location: "Peoria, IL",
+    description: [
+      "Currently working on developing innovative software solutions.",
+      "Contributing to large-scale projects with a focus on performance optimization.",
+      "Collaborating closely with cross-functional teams to ensure product quality.",
+      "Adhering to clean code practices and modern development techniques.",
+    ],
+  },
+  {
+    designation: "Full Stack Developer",
+    company: "Tata Consultancy Services",
+    startDate: "Aug 2021",
+    endDate: "Mar 2025",
+    isCurrentJob: false,
+    location: "Hyderabad, India",
+    description: [
+      "Developed over 5 web applications with seamless backend API integration.",
+      "Streamlined workflows by eliminating redundant data, boosting efficiency.",
+      "Improved website loading time by 80% through image optimization, minimizing main-thread work, and implementing SEO strategies.",
+      "Resolved 100+ bugs and conducted thorough code reviews.",
+      "Demonstrated expertise in both backend and frontend development.",
+    ],
+  },
+  {
+    designation: "Full Stack Development Intern",
+    company: "WOXA.AI",
+    startDate: "May 2020",
+    endDate: "Jul 2021",
+    isCurrentJob: false,
+    location: "Hyderabad, India",
+    description: [
+      "Revamped and enhanced 3+ mobile apps using Flutter.",
+      "Deployed RESTful APIs for seamless app-server integration.",
+      "Integrated Google AdMob to effectively monetize applications.",
+      "Contributed to boosting app functionality and revenue generation.",
+    ],
+  },
+  {
+    designation: "Web Development Intern",
+    company: "StorKode",
+    startDate: "Jul 2019",
+    endDate: "Apr 2020",
+    isCurrentJob: false,
+    location: "Remote",
+    description: [
+      "Spearheaded website development and enhancements for company portals.",
+      "Prioritized clean, reusable code with modern tech stacks.",
+      "Efficiently managed multiple tasks with minimal supervision.",
+      "Collaborated with senior team members to meet and exceed project goals.",
+    ],
+  },
+];
+
+export default experiences;
